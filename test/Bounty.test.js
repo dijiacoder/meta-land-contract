ff397d46ed49e45027cd208db79a866c02da8193
Dijia <dijiacoder@163.com>
2025-06-07 19:26:40 +0800

    Use BountyFactory createBounty

--- test/Bounty.test.js
+++ test/Bounty.test.js
@@ -8,6 +8,8 @@ describe("Bounty", function () {
     let bountyFactory, bountyFactoryProxy, bountyBeacon, bountyV1, bountyV2;
     let erc20A, erc20B;
     let provider;
+    let ERC20Token;
+    let erc20TokenAddress;
 
     beforeEach(async function () {
         FactoryStore = await ethers.getContractFactory("FactoryStore");
@@ -54,6 +56,13 @@ describe("Bounty", function () {
 
         // 使用逻辑合约ABI
         bountyFactory = await BountyFactory.attach(bountyFactoryProxy)
+
+        // deploy ERC20Token
+        const ERC20TokenFactory = await ethers.getContractFactory("ERC20Token");
+        ERC20Token = await ERC20TokenFactory.deploy("ERC20Token", "TESTToken");
+        await ERC20Token.deployed();
+        erc20TokenAddress = ERC20Token.address;
+        // console.log("ERC20Token address: ", erc20TokenAddress)
     });
 
     describe("BountyFactory", function () {
@@ -112,6 +121,150 @@ describe("Bounty", function () {
             expect(oldBeacon).to.eq(newBeacon);
         });
     });
+
+    describe("Use BountyFactory createBounty", function () {
+        const applyDeadline = Math.floor(Date.now() / 1000) + 7 * 3600; // 7 days
+        const founderDepositAmount = ethers.utils.parseEther("1.0");
+        const applicantDepositAmount = ethers.utils.parseEther("0.1");
+
+        it("Use ETH create bounty (_founderDepositAmount = 0)", async function () {
+            const tx = await bountyFactory.connect(addr1).createBounty(
+                ethers.constants.AddressZero,
+                0,
+                applicantDepositAmount,
+                applyDeadline
+            );
+
+            const receipt = await tx.wait();
+            const event = receipt.logs.map(log => {
+                try {
+                    return bountyFactory.interface.parseLog(log);
+                } catch (e) {
+                    return null;
+                }
+            }).find(e => e?.name === 'Created');
+
+            expect(event).to.not.be.undefined;
+            expect(event.args.founder).to.equal(addr1.address);
+            // console.log("event.args.bounty:", event.args.bounty);
+            expect(event.args.bounty).to.not.equal(ethers.constants.AddressZero);
+
+            const children = await bountyFactory.children();
+            expect(children.length).to.equal(1);
+            expect(children[0]).to.equal(event.args.bounty);
+        });
+
+        it("Use ETH create bounty (_founderDepositAmount > 0)", async function () {
+            const tx = await bountyFactory.connect(addr1).createBounty(
+                ethers.constants.AddressZero,
+                founderDepositAmount,
+                applicantDepositAmount,
+                applyDeadline,
+                { value: founderDepositAmount }
+            );
+
+            const receipt = await tx.wait();
+            const event = receipt.logs.map(log => {
+                try {
+                    return bountyFactory.interface.parseLog(log);
+                } catch (e) {
+                    return null;
+                }
+            }).find(e => e?.name === 'Created');
+
+            expect(event).to.not.be.undefined;
+            expect(event.args.founder).to.equal(await addr1.getAddress());
+            const bountyAddress = event.args.bounty;
+            // console.log("bountyAddress:", bountyAddress);
+
+            const bounty = await ethers.getContractAt("Bounty", bountyAddress);
+            const vaultAddress = await bounty.connect(addr1).vaultAccount();
+            // console.log("vaultAddress:", vaultAddress);
+            const balance = await ethers.provider.getBalance(vaultAddress);
+            // console.log("balance:", balance);
+            expect(balance).to.equal(founderDepositAmount);
+        });
+
+        it("Use ERC20 create bounty", async function () {
+            await ERC20Token.mint(addr1.address, founderDepositAmount);
+            await ERC20Token.connect(addr1).approve(bountyFactory.address, founderDepositAmount);
+
+            const tx = await bountyFactory.connect(addr1).createBounty(
+                erc20TokenAddress,
+                founderDepositAmount,
+                applicantDepositAmount,
+                applyDeadline
+            );
+
+            const receipt = await tx.wait();
+            const event = receipt.logs.map(log => {
+                try {
+                    return bountyFactory.interface.parseLog(log);
+                } catch (e) {
+                    return null;
+                }
+            }).find(e => e?.name === 'Created');
+
+            expect(event).to.not.be.undefined;
+            expect(event.args.founder).to.equal(addr1.address);
+
+            const bounty = await ethers.getContractAt("Bounty", event.args.bounty);
+            const vaultAddress = await bounty.connect(owner).vaultAccount();
+            // console.log("vaultAddress:", vaultAddress);
+            const balance = await ERC20Token.connect(addr1).balanceOf(vaultAddress);
+            // console.log("balance:", balance);
+            expect(balance).to.equal(founderDepositAmount);
+        });
+
+        it("Should create failed (Applicant cutoff date is expired)", async function () {
+
+            const expiredDeadline = Math.floor(Date.now() / 1000) - 3600;
+            await expect(bountyFactory.connect(addr1).createBounty(
+                ethers.constants.AddressZero,
+                0,
+                applicantDepositAmount,
+                expiredDeadline
+            )).to.be.revertedWith("Applicant cutoff date is expired");
+        });
+
+        it("Should create failed (msg.value is not valid)", async function () {
+
+            const founderDepositAmount2 = ethers.utils.parseEther("2.0");
+            await expect(bountyFactory.connect(addr1).createBounty(
+                ethers.constants.AddressZero,
+                founderDepositAmount,
+                applicantDepositAmount,
+                applyDeadline,
+                { value: founderDepositAmount2 }
+            )).to.be.revertedWith("msg.value is not valid");
+        });
+
+        it("Should create failed (Deposit token balance is insufficient)", async function () {
+            
+            await ERC20Token.mint(addr2.address, ethers.utils.parseEther("0.5"));
+            await ERC20Token.connect(addr2).approve(bountyFactory.address, ethers.utils.parseEther("0.5"));
+            await expect(bountyFactory.connect(addr2).createBounty(
+                erc20TokenAddress,
+                ethers.utils.parseEther("1.0"),
+                applicantDepositAmount,
+                applyDeadline,
+                { value: ethers.utils.parseEther("1.0") }
+            )).to.be.revertedWith("Deposit token balance is insufficient");
+        });
+
+        it("Should create failed (Deposit token allowance is insufficient)", async function () {
+            
+            await ERC20Token.mint(addr2.address, ethers.utils.parseEther("1.0"));
+            await ERC20Token.connect(addr2).approve(bountyFactory.address, ethers.utils.parseEther("0.5"));
+            await expect(bountyFactory.connect(addr2).createBounty(
+                erc20TokenAddress,
+                ethers.utils.parseEther("1.0"),
+                applicantDepositAmount,
+                applyDeadline,
+                { value: ethers.utils.parseEther("1.0") }
+            )).to.be.revertedWith("Deposit token allowance is insufficient");
+        });
+    })
 
     describe("Bounty", function () {
         it("Should initialize successfully", async function () {
@@ -191,4 +344,4 @@ describe("Bounty", function () {
             expect(await bounty2V2Ins.isUpgraded()).to.eq(1000);
         });
     });
-}); 
+});
